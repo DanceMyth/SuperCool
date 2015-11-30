0bddce149a16393b6730c87fdd69fe0d99cda11b
Dan Smith <Dancemyth@icloud.com>
2015-11-30 23:21:40 +0000

    first commit for supercool

--- Supercool/ViewController.swift
+++ Supercool/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var bg: UIImageView!
+    @IBOutlet weak var uncool: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeCool(sender: AnyObject) {
+        Logo.hidden = false
+        bg.hidden = false
+        uncool.hidden = true
+        
+    }
 
 }
 
